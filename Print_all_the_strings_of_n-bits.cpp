440070e0248815137a90de933a3b2003d4be86e0
Anshul Gupta <anshulmgupta@gmail.com>
2017-10-05 00:10:00 -0700

    Print all the strings of n-bits

--- Print_all_the_strings_of_n-bits.cpp
+++ Print_all_the_strings_of_n-bits.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 
@@ -16,11 +16,32 @@ void print_string(int n, int idx, string str){
     str.pop_back();
 }
 
+void print_string_one(int n, int idx, vector<char> str){
+    if(n == idx){
+        for(auto d: str)
+            cout << d;
+        cout << endl;
+        return;
+    }
+    str[idx] = '0';
+    print_string_one(n, idx+1, str);
+    str[idx] = '1';
+    print_string_one(n, idx+1, str);
+}
+
 void test_case_one(){
     int no_bits = 3;
     print_string(no_bits, 0, "");
 }
 
+void test_case_two(){
+    int no_bits = 3;
+    vector<char> str(no_bits);
+    print_string_one(no_bits, 0, str);
+}
+
 int main(){
     test_case_one();
+    cout << "Test Case 2" << endl;
+    test_case_two();
 }
